eddae5d81ac0d66af0f3acbc0b8c70fc2f1be32a
Dima <dbjenov@gmail.com>
2021-04-09 11:57:15 +0300

    Header-to mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 var btn = document.getElementById("theme-button");
+var logo = document.getElementById("logo");
 var bg = document.getElementById("header");
+var list = document.getElementById("menu__link");
+var preview = document.getElementById("preview-prew");
+var holder = document.getElementById('holder');
+
+
 
 
 btn.addEventListener("click", function () { ChangeTheme(); });
@@ -9,6 +15,10 @@ function ChangeTheme()
 {
     bg.classList.toggle('light');
     btn.classList.toggle('light-btn');
+    list.classList.toggle('link-light');
+    logo.classList.toggle('light-logo');
+    preview.classList.toggle('light-prev');
+    holder.classList.toggle('light-prev-text');
 }
 
 
